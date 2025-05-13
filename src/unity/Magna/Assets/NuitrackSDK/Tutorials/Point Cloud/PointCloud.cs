46ed99111c9b31cbd6b26dc6c74cab55d43f2e42
Gianni Bautista <bautistag@vcu.edu>
2025-05-12 22:40:47 -0400

    changes
    test

--- src/unity/Magna/Assets/NuitrackSDK/Tutorials/Point Cloud/PointCloud.cs
+++ src/unity/Magna/Assets/NuitrackSDK/Tutorials/Point Cloud/PointCloud.cs
@@ -1,25 +1,22 @@
 ﻿using UnityEngine;
 
-
 namespace NuitrackSDK.Tutorials.PointCloud
 {
     [AddComponentMenu("NuitrackSDK/Tutorials/Point Cloud/Point Cloud")]
     public class PointCloud : MonoBehaviour
     {
-        [SerializeField] Material depthMat = null, colorMat = null; //materials for depth and color output
-
-        ulong lastFrameID = ulong.MaxValue;
-
+        [SerializeField] Material depthMat = null; // Material (optional, not required for coloring each cube)
         [SerializeField] int hRes;
-        int frameStep;
-
-        [SerializeField] Color defaultColor;
+        [SerializeField] Color defaultColor = Color.gray;
         [SerializeField] GameObject pointMesh;
         [SerializeField] float meshScaling = 1f;
-        float depthToScale; //Size of the "points", depending on the range (than the cubes further, the larger they are)
-        Texture2D depthTexture, rgbTexture;
+
+        ulong lastFrameID = ulong.MaxValue;
+        int frameStep;
+        float depthToScale;
+
+        Texture2D depthTexture;
         Color[] depthColors;
-        Color[] rgbColors;
         GameObject[] points;
 
         bool initialized = false;
@@ -33,22 +30,21 @@ namespace NuitrackSDK.Tutorials.PointCloud
         {
             initialized = true;
 
-            nuitrack.OutputMode mode = NuitrackManager.DepthSensor.GetOutputMode(); //Returns the structure in which there is resolution, FPS and FOV of the sensor
+            nuitrack.OutputMode mode = NuitrackManager.DepthSensor.GetOutputMode();
 
             frameStep = mode.XRes / hRes;
-            if (frameStep <= 0) frameStep = 1; // frameStep must be bigger than 0
+            if (frameStep <= 0) frameStep = 1;
             hRes = mode.XRes / frameStep;
 
             InitMeshes(
-              ((mode.XRes / frameStep)), //Width
-              ((mode.YRes / frameStep)), //Height
+              (mode.XRes / frameStep),  // Width
+              (mode.YRes / frameStep),  // Height
               mode.HFOV);
         }
 
         void InitMeshes(int cols, int rows, float hfov)
         {
             depthColors = new Color[cols * rows];
-            rgbColors = new Color[cols * rows];
             points = new GameObject[cols * rows];
 
             depthTexture = new Texture2D(cols, rows, TextureFormat.RFloat, false);
@@ -56,19 +52,13 @@ namespace NuitrackSDK.Tutorials.PointCloud
             depthTexture.wrapMode = TextureWrapMode.Clamp;
             depthTexture.Apply();
 
-            rgbTexture = new Texture2D(cols, rows, TextureFormat.ARGB32, false);
-            rgbTexture.filterMode = FilterMode.Point;
-            rgbTexture.wrapMode = TextureWrapMode.Clamp;
-            rgbTexture.Apply();
-
-            depthMat.mainTexture = depthTexture;
-            colorMat.mainTexture = rgbTexture;
+            if (depthMat != null)
+                depthMat.mainTexture = depthTexture;
 
             int pointId = 0;
-
-            for (int i = 0; i < rgbTexture.height; i++)
+            for (int i = 0; i < rows; i++)
             {
-                for (int j = 0; j < rgbTexture.width; j++)
+                for (int j = 0; j < cols; j++)
                 {
                     points[pointId++] = Instantiate(pointMesh, transform);
                 }
@@ -79,19 +69,18 @@ namespace NuitrackSDK.Tutorials.PointCloud
         {
             if (NuitrackManager.DepthFrame != null)
             {
-                nuitrack.DepthFrame depthFrame = NuitrackManager.DepthFrame;
-                nuitrack.ColorFrame colorFrame = NuitrackManager.ColorFrame;
-
+                var depthFrame = NuitrackManager.DepthFrame;
                 bool haveNewFrame = (lastFrameID != depthFrame.ID);
+
                 if (haveNewFrame)
                 {
-                    ProcessFrame(depthFrame, colorFrame);
+                    ProcessFrame(depthFrame);
                     lastFrameID = depthFrame.ID;
                 }
             }
         }
 
-        void ProcessFrame(nuitrack.DepthFrame depthFrame, nuitrack.ColorFrame colorFrame)
+        void ProcessFrame(nuitrack.DepthFrame depthFrame)
         {
             int pointIndex = 0;
 
@@ -99,32 +88,37 @@ namespace NuitrackSDK.Tutorials.PointCloud
             {
                 for (int j = 0; j < depthFrame.Cols; j += frameStep)
                 {
-                    //take depth from the frame and put it into the depthColors array
-                    depthColors[pointIndex].r = depthFrame[i, j] / 16384f;
+                    ushort depthVal = depthFrame[i, j];
 
-                    //take from the rgb frame a color and put it into an array rgbColors
-                    Color rgbCol = defaultColor;
-                    if (colorFrame != null)
-                        rgbCol = new Color32(colorFrame[i, j].Red, colorFrame[i, j].Green, colorFrame[i, j].Blue, 255);
-                    rgbColors[pointIndex] = rgbCol;
-
-                    //Hide the "point" if its depth is 0
-                    if (depthFrame[i, j] == 0)
+                    if (depthVal == 0)
+                    {
                         points[pointIndex].SetActive(false);
+                    }
                     else
                     {
-                        points[pointIndex].GetComponent<Renderer>().material.color = rgbCol;
-
-                        //change of position of the point (cube) in Z (depth)
-                        Vector3 newPos = NuitrackManager.DepthSensor.ConvertProjToRealCoords(j, i, depthFrame[i, j]).ToVector3();
-
                         points[pointIndex].SetActive(true);
+
+                        // Depth to world coordinates
+                        Vector3 newPos = NuitrackManager.DepthSensor
+                            .ConvertProjToRealCoords(j, i, depthVal)
+                            .ToVector3();
+
                         points[pointIndex].transform.position = newPos;
 
-                        float distancePoints = Vector3.Distance(newPos, NuitrackManager.DepthSensor.ConvertProjToRealCoords(j + 1, i, depthFrame[i, j]).ToVector3()); //Distance between points
-                        depthToScale = distancePoints * depthFrame.Cols / hRes; //calculate the size of cubes as a function of depth
+                        // Scale based on distance
+                        float distancePoints = Vector3.Distance(
+                            newPos,
+                            NuitrackManager.DepthSensor.ConvertProjToRealCoords(j + 1, i, depthVal).ToVector3());
 
+                        depthToScale = distancePoints * depthFrame.Cols / hRes;
                         points[pointIndex].transform.localScale = Vector3.one * meshScaling * depthToScale;
+
+                        // Color based on depth (Red = close, Blue = far)
+                        float normalized = Mathf.InverseLerp(500f, 4000f, depthVal); // Adjust range to your sensor
+                        Color depthColor = Color.HSVToRGB(0.66f - normalized * 0.66f, 1f, 1f); // Blue → Red gradient
+                        points[pointIndex].GetComponent<Renderer>().material.color = depthColor;
+
+                        depthColors[pointIndex] = new Color(depthVal / 16384f, 0f, 0f, 1f); // Optional grayscale
                     }
 
                     ++pointIndex;
@@ -132,10 +126,7 @@ namespace NuitrackSDK.Tutorials.PointCloud
             }
 
             depthTexture.SetPixels(depthColors);
-            rgbTexture.SetPixels(rgbColors);
-
             depthTexture.Apply();
-            rgbTexture.Apply();
         }
     }
 }
